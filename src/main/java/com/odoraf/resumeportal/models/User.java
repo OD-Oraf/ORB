c708666a9dd240259e5ae1ca44865a5288d37b29
OD-Oraf <43830269+OD-Oraf@users.noreply.github.com>
2021-07-29 13:23:45 -0400

    adding User and repository JPA infastructure

---
+++ src/main/java/com/odoraf/resumeportal/models/User.java
@@ -0,0 +1,60 @@
+package com.odoraf.resumeportal.models;
+
+import javax.persistence.*;
+
+//Create table called user in database
+@Entity
+@Table(name = "User")
+public class User {
+    //Primary key
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String userName;
+    private String password;
+    //User needs to be active at the point of login
+    private boolean active;
+    //User vs admin role
+    private String roles;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+}
