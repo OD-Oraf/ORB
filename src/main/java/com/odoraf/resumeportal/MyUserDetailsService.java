4346309808d47a38331381b472f9a61fc4c5350a
OD-Oraf <43830269+OD-Oraf@users.noreply.github.com>
2021-07-31 07:46:01 -0400

    adding user JPA support for Spring Security

---
+++ src/main/java/com/odoraf/resumeportal/MyUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.odoraf.resumeportal;
+
+import com.odoraf.resumeportal.models.MyUserDetails;
+import com.odoraf.resumeportal.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+//Implementation of Spring UserDetailsService Interface
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    //UserDetails is returned bean
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUserName(userName);
+
+        user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + userName));
+
+        //if found, convert to user details class, spring security user details
+        //Look up map for more details
+        return user.map(MyUserDetails::new).get();
+    }
+}
