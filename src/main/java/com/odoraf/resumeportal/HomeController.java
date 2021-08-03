24bb2d769bb71a0cd0082b061c45296d520b6864
OD-Oraf <43830269+OD-Oraf@users.noreply.github.com>
2021-08-03 11:04:08 -0400

    added user profile entity and fetch to show theme and summary

--- src/main/java/com/odoraf/resumeportal/HomeController.java
+++ src/main/java/com/odoraf/resumeportal/HomeController.java
@@ -1,5 +1,6 @@
 package com.odoraf.resumeportal;
 
+import com.odoraf.resumeportal.models.Job;
 import com.odoraf.resumeportal.models.MyUserDetails;
 import com.odoraf.resumeportal.models.UserProfile;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Optional;
 
 @Controller
@@ -20,7 +23,31 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(){
-        return "Hello";
+        UserProfile profile1 = new UserProfile();
+        profile1.setId(1);
+        profile1.getDesignation("Designation");
+        profile1.setUserName("einstein");
+        profile1.setFirstName("Albert");
+        profile1.setLastName("Einstein");
+        profile1.setTheme(1);
+
+        Job job1 = new Job();
+        job1.setCompany("Company 1");
+        job1.getDesignation("Designation");
+        job1.setId(1);
+        job1.setStartDate(LocalDate.of(2020, 1,1));
+        job1.setEndDate(LocalDate.of(2020,3,1));
+
+        Job job2 = new Job();
+        job2.setCompany("Company 2");
+        job2.getDesignation("Designation");
+        job2.setId(2);
+        job2.setStartDate(LocalDate.of(2019, 5,1));
+        job2.setEndDate(LocalDate.of(2020,1,1));
+
+        profile1.setJobs(Arrays.asList(job1, job2));
+        userProfileRepository.save(profile1);
+        return "profile";
     }
 
     @GetMapping("/edit")
@@ -37,6 +64,7 @@ public class HomeController {
         model.addAttribute("userId", userId);
         UserProfile userProfile = userProfileOptional.get();
         model.addAttribute("userProfile", userProfile);
+        System.out.println(userProfile.getJobs());
 
         //return statement links to templates in resources due to tymleaf configuration
         return "profile-templates/" + userProfile.getId() + "/index";
--- src/main/java/com/odoraf/resumeportal/models/UserProfile.java
+++ src/main/java/com/odoraf/resumeportal/models/UserProfile.java
@@ -2,6 +2,8 @@ package com.odoraf.resumeportal.models;
 
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table
@@ -13,6 +15,67 @@ public class UserProfile {
     private String userName;
     private int theme;
     private String summary;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String designation;
+
+    //One profile can have many jobs
+    //When a profile delete, the jobs delete
+    @OneToMany(cascade = CascadeType.ALL,
+        orphanRemoval = true)
+    @JoinColumn(name = "job_id")
+    List<Job> jobs = new ArrayList<>();
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDesignation(String designation) {
+        return this.designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
 
     public int getId() {
         return id;
